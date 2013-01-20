f2af7a21eed859b598ff246e5dfd11a826eafa31
Piotr Olma <perdo.olma@gmail.com>
2013-01-20 14:35:40 +0100

    Add random partitioner

---
+++ src/AlgorithmPartitionRandom.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pdr
+ * Date: 1/20/13
+ * Time: 2:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlgorithmPartitionRandom implements Algorithm_partition {
+    @Override
+    public int[][] run(Problem_instance pi) {
+        Random rnd = new Random();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int picked = 0;
+        boolean[] alreadyPicked = new boolean[pi.n];
+        while (picked < pi.n) {
+            int currentTotal = 0;
+            List<Integer> current = new ArrayList<Integer>();
+            while (true) {
+                int r = rnd.nextInt(pi.n);
+                if(!alreadyPicked[r]){
+                    if(currentTotal + pi.w[r] <= pi.W){
+                        alreadyPicked[r] = true;
+                        ++picked;
+                        current.add(r);
+                    }
+                    else{
+                        result.add(current);
+                        break;
+                    }
+                }
+            }
+        }
+        //convert to int[][]
+        int [][] resultArray = new int[result.size()][];
+        int index  = 0;
+        for(List a: result){
+            resultArray[index++] = listToArray(a);
+        }
+        return resultArray;
+    }
+
+    private int[] listToArray(List<Integer> list){
+        int [] result = new int[list.size()];
+        for(int i =0; i<list.size(); ++i)
+            result[i] = list.get(i);
+        return result;
+    }
+}
